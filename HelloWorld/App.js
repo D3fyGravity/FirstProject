a07de7281f4ae46af2cb305c000a1bc40f788920
D3fyGravity <paul.toastman@gmail.com>
2019-04-07 18:19:22 +1000

    changes

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+class Greeting extends Component {
+  render() {
+    return (
+      <View style={{allignItems: 'center'} }> 
+        <text> Hello {this.props.name} </text>
+      </View>
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <View style={styles.container}>
-        <Text> Testing Github repository
-        </Text>
+      <View style={{allignItems= 'center'}}>
+        <Greeting name='Paul' />
+        <Greeting name= 'Tyler' /> 
+        <Greeting name= 'Lex' />
         <Image source= {pic} style={{width: 150, height: 150}}/>
       </View>
     );

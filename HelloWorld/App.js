efa235c210ea6b30be23ba0bbffd52c997eb5733
D3fyGravity <paul.toastman@gmail.com>
2019-04-07 17:29:45 +1000

    added new text

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HelloWorld! 
+        <Text> Testng Github repository
         </Text>
       </View>
     );
